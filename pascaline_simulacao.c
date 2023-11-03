286e88d7c8eb3621045cb1ba01c2c3b3437b2f7d
victxl <victxl@gmail.com>
2023-11-03 19:22:21 -0300

    Continuar

--- pascaline_simulacao.c
+++ pascaline_simulacao.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 int main() {
  int distancia, velocidade;
- printf("Digite a distância da viagem em milhas nauticas: ");
+ printf("Digite a distancia da viagem em milhas nauticas: ");
  scanf("%d", &distancia);
- printf("Digite a velocidade média do navio em nos: ");
+ printf("Digite a velocidade media do navio em nos: ");
  scanf("%d", &velocidade);
  int tempo = distancia / velocidade;
  printf("Sua viagem levara aproximadamente %d horas.\n", tempo);
  return 0;
 }
+
